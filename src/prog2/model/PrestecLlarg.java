9802d64bab3737d4233642da3d3906c980af85da
anni007199 <anniqixu@gmail.com>
2026-04-23 17:54:39 +0200

    acabat

--- src/prog2/model/PrestecLlarg.java
+++ src/prog2/model/PrestecLlarg.java
@@ -1,4 +1,19 @@
 package prog2.model;
 
-public class PrestecLlarg {
+import java.util.Date;
+
+public class PrestecLlarg extends Prestec{
+    // Atributs
+    // "140000L" vol dir 140 segons
+    private static final long DURADA_PRESTEC_LLARGS = 140000L;
+
+    // Constructor
+    public PrestecLlarg(Exemplar exemplar, Usuari usuari, Date dataCreacio){
+        super(exemplar,usuari,dataCreacio);
+    }
+
+    @Override
+    public long duradaPrestec(){
+        return DURADA_PRESTEC_LLARGS;
+    }
 }
--- src/prog2/model/PrestecNormal.java
+++ src/prog2/model/PrestecNormal.java
@@ -1,4 +1,19 @@
 package prog2.model;
 
-public class PrestecNormal {
+import java.util.Date;
+
+public class PrestecNormal extends Prestec{
+    // Atributs
+    // "70000L" vol dir 70 segons
+    private static final long DURADA_PRESTEC_NORMAL = 70000L;
+
+    // Constructor
+    public PrestecNormal(Exemplar exemplar, Usuari usuari, Date dataCreacio){
+        super(exemplar,usuari,dataCreacio);
+    }
+
+    @Override
+    public long duradaPrestec(){
+        return DURADA_PRESTEC_NORMAL;
+    }
 }
