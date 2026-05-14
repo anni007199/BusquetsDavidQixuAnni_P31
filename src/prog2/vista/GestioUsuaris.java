c5936e33bac4b1f07b08e5e73ae6726a4ada0907
anni007199 <anniqixu@gmail.com>
2026-05-14 13:44:45 +0200

    començat

--- src/prog2/vista/GestioUsuaris.java
+++ src/prog2/vista/GestioUsuaris.java
@@ -1,4 +1,22 @@
 package prog2.vista;
 
-public class GestioUsuaris {
+import javax.swing.*;
+
+public class GestioUsuaris extends JFrame{
+    private JPanel panelGestioUsuaris;
+    private JTextField txtNom;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            GestioUsuaris gestioUsuaris = new GestioUsuaris();
+            gestioUsuaris.setVisible(true);
+        });
+    }
+    public GestioUsuaris(){
+        setTitle("GestioUsuaris GUI");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(panelGestioUsuaris);
+        setSize(500,400);
+        setLocationRelativeTo(null);
+    }
 }
